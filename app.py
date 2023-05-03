6dfc6ce68375d687299103dd6335a0c4dc7e07e2
Evert Diaz <evertdiazb@gmail.com>
2023-05-03 14:11:19 -0500

    Lesson 3: Local State

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ class MyState:
     stack_type=TealType.uint64,
     default=Int(999)
   )
+  local_sum = LocalStateValue(
+    stack_type=TealType.uint64,
+    default=Int(999)
+  )
 
-app = Application("Calculator", state=MyState())
-
-@app.create
-def create() -> Expr:
-  return app.initialize_global_state()
+app = Application("HelloWorld", state=MyState()).apply(
+  unconditional_create_approval, initialize_global_state=True).apply(
+  unconditional_opt_in_approval, initialize_local_state=True)
 
 def sub_add(a: abi.Uint64, b: abi.Uint64) -> Expr:
   return a.get() + b.get()
@@ -22,12 +24,17 @@ def add(a: abi.Uint64, b: abi.Uint64, *, output: abi.Uint64) -> Expr:
   result = sub_add(a,b)
   return Seq(
     app.state.global_sum.set(result),
+    app.state.local_sum[Txn.sender()].set(result),
     output.set(result)
   )
 
 @app.external
 def read_global(*,output: abi.Uint64) -> Expr:
   return output.set(app.state.global_sum.get())
+
+@app.external
+def read_local(*,output: abi.Uint64) -> Expr:
+  return output.set(app.state.local_sum[Txn.sender()].get())
 
 
 if __name__ == "__main__":
